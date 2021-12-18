710d75534c4a2fa8421387656fabe02afd527431
brittanypiotrowski <81540634+brittanypiotrowski@users.noreply.github.com>
2021-12-18 16:20:10 -0500

    Added functionality to the Back to Top button in JavaScript. Also added CSS styling to the button for hover effect and easy accessability.

--- main.js
+++ main.js
@@ -90,3 +90,23 @@ dark_mode_toggle.onclick = () => {
     document.querySelector('body').classList.toggle('light')
     document.querySelector('body').classList.toggle('dark')
 }
+
+
+var topbutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topbutton.style.display = "block";
+    } else {
+        topbutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
